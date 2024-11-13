40eb1a879aa7553df0fb534a78815e974600b7a6
Karnzzz <anotherzz001@gmail.com>
2024-11-13 09:43:05 +0700

    starting

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    // Add any other folders where JSX files might be present
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
